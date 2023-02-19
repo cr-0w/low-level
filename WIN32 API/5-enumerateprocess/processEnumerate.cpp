8fb515a3ba26df1682a2ec8aa605cc7a3704eebb
crow <59679082+cr-0w@users.noreply.github.com>
2023-02-19 10:37:27 -0500

    Create processEnumerate.cpp

---
+++ WIN32 API/5-enumerateprocess/processEnumerate.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <windows.h>
+
+STARTUPINFO startInfo = { };
+
+//HANDLE hThread;
+//HANDLE hProcess;
+//DWORD dwThreadID;
+//DWORD dwProcessID;
+
+PROCESS_INFORMATION procInfo = {
+    //hProcess,
+    //hThread,
+    //dwProcessID,
+    //dwThreadID
+};
+
+int main() {
+
+    printf("\n[+] spawning process: notepad.exe");
+
+    if (!CreateProcessW(
+        L"C:\\Windows\\System32\\notepad.exe",
+        NULL,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &startInfo,
+        &procInfo
+    )) {
+
+        printf("\n[!] process couldn't be created. you're on your own. good luck.");
+        exit(1);
+    }
+
+    printf("\n[+] process spawned!");
+    printf("\n[+] process ID: %ld", procInfo.dwProcessId);
+    printf("\n[+] thread ID: %ld", procInfo.dwThreadId);
+
+}
