b0ef555176d743100acd8cc1fdc194b476f87e84
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-02 23:29:04 -0500

    Update winapi.c

--- C/winapi/winapi.c
+++ C/winapi/winapi.c
@@ -1,6 +1,28 @@
+#include <stdio.h>
 #include <windows.h>
 
+typedef struct {
+	int age;
+	char name[32];
+	float balance;
+} person;
+
+person newPerson = {31, "bernard black", 120.0};
+
+void msgbx() {
+	float balance = newPerson.balance;
+	(balance >= 100) ? MessageBoxW(NULL, L"you have enough to enter the book shop!", L"COME IN!", MB_OK | MB_ICONINFORMATION) : MessageBoxW(NULL, L"you don't have enough to enter the book shop!", L"HALT!", MB_OK | MB_ICONHAND);
+}
+
+void userInfo() {
+	printf("[+] %s, welcome!", newPerson.name);
+	printf("\n[+] you are %d years old.", newPerson.age);
+	printf("\n[!] let's see if you have enough to enter black books. currently it costs $100.");
+	printf("\n[+] you have $%.2f dollars in your bank account.\n", newPerson.balance);
+}
+
 int main(void) {
-    MessageBoxW(NULL, L"LET'S GO, WIN32 API FTW!", L"IT WORKS!", MB_OK | MB_ICONINFORMATION);
-    return 0;
+	userInfo();
+	msgbx();
+	return EXIT_SUCCESS;
 }
